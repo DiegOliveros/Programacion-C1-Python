895305dd1eb39fa06bfbaa75b3706a45795faf1f
DiegOliveros <diegoo.ivan@gmail.com>
2022-05-26 18:48:58 -0500

    Update correos.py

--- Código Clase/S6/correos.py
+++ Código Clase/S6/correos.py
@@ -5,16 +5,18 @@ Created on Thu May 26 18:16:20 2022
 #  
 # Diego Iván Oliveros Acosta @scalapp.co   @author: ITOS
 # 
-# [name]   [Trabajando con lista de correo]
+# [name]   [Trabajando con listas de correo]
 # =============================================================================
-# 
+# Cree una función email_list que reciba un diccionario, que contenga
+ nombres de dominio como claves y una lista de usuarios como valores.
+ Desarrolle el código para generar una lista que contenga direcciones de correo electrónico completas (por ejemplo, diana.prince@gmail.com). Obtenga los datos de un json y retorne un xlsx (csv) con una nueva tabla. 
 # =============================================================================
 """
 
-diccionario={'hotmail.com':["leafarsoft", "andres", "juan"]}   #calve, valor
+diccionario={'hotmail.com':["leafarsoft", "andres", "juan"]}   #clave, valor
 diccionario={'hotmail.com':["leafarsoft", "andres", "juan"],
              "gmail.com":["Andrés", "Rafael", "Jorge", "Sebastian", "Yeison", "luisa"],
-             "udea.edu.co":["Andrés", "Rafael", "Jorge", "Sebastian", "Yeison", "luisa"]}   #calve, valor
+             "udea.edu.co":["Andrés", "Rafael", "Jorge", "Sebastian", "Yeison", "luisa"]}   #clave, valor
 
 
 #Paso 1: 
