628b3512b3db6c1dd4576fa1bb8302111658b034
DiegOliveros <diegoo.ivan@gmail.com>
2022-05-28 10:03:40 -0500

    Update fecha.py

--- Código Clase/S6/fecha.py
+++ Código Clase/S6/fecha.py
@@ -5,34 +5,40 @@ Created on Sat May 28 08:20:58 2022
 #  
 # Diego Iván Oliveros Acosta @scalapp.co   @author: ITOS
 # 
-# [name]   []
+# [fecha.py]   [manipulando archivos csv]
 # =============================================================================
-# 
+# https://github.com/DiegOliveros/MISION-TIC-2020/blob/main/Retos/Taller%202.pdf
 # =============================================================================
 """
 
 import datetime as dt
 import pytz
 import pandas as pd
-
+#algunas aclaraciones sobre el modulo datetime:
 fecha = dt.datetime.now()
 
+#posibles formatos
 print(fecha, type(fecha))
 print(fecha.year)
 print(fecha.strftime("%c"))
 print(fecha.strftime("%X"))
 print(fecha.strftime("%x"))
-
+#otros formatos https://strftime.org/
 
 print('Todas las posibles zonas horarias:')
 for timeZone in pytz.all_timezones:
-    print(timeZone)
-    
+    print(timeZone)   #todas las posibles zonas horarias
+
+#Realizando un cambio de zona horaria  
 timezone = pytz.timezone("America/Lima")   #America/Lima en nuestro caso en lugar de Europe/Dublin"
 fecha = dt.datetime.now(timezone)
-
+#Imprimiendo el cambio de zona horaria 
 print(fecha, type(fecha))
 print(fecha.year)
+
+# Desarrollo del ejercicio 
+# la descripcción la encuentro en la siguiente url:
+#https://github.com/DiegOliveros/MISION-TIC-2020/blob/main/Retos/Taller%202.pdf
 
 i=10
 diccionario={}
@@ -50,7 +56,7 @@ while i !=0:
          longitud=input("ingrese la longitud: ")
          diccionario[fecha]=[altura,latitud,longitud]
 
-    
+#formar cadena y guardar
 print("la sesión ha terminado")    
 for x in diccionario.keys():
     print(x)    
@@ -59,7 +65,7 @@ for x in diccionario.keys():
     file.write(str(x)+","+str(diccionario[x][0])+","+str(diccionario[x][1])+","+str(diccionario[x][2]))
     file.close()
 
-
+#hacemos lectura del archivo
 
 file = open("registrotopográfico.csv","r")   #leer
 print(file.read())  #texto compleo 
